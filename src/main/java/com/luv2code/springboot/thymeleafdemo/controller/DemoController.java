a0160c302e25e666254522548f86c27741d22135
DaynaLau <73656477+DaynaLau@users.noreply.github.com>
2021-04-26 16:49:57 -0400

    Adding src files

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,35 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	
+	// create a mapping for "/hello"
+	
+	@GetMapping("/hello")
+	public String sayHello(Model theModel) {
+		
+		theModel.addAttribute("theDate", new java.util.Date());
+		
+		return "helloworld";
+	}
+	
+	@GetMapping("/showMyLoginPage")
+	public String login() {
+		
+		return "fancy-login";
+	}
+	
+	// add request mapping for /access-denied
+	
+		@GetMapping("/access-denied")
+		public String showAccessDenied() {
+			
+			return "access-denied";
+			
+		}
+	
+}
